f697b6620af3ce8831671e22daaac3b00025d358
tani-cat <tani.tvet2749@gmail.com>
2022-06-04 14:19:43 +0900

    change url

--- lunch_gacha/urls.py
+++ lunch_gacha/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = 'lunch_gacha'
 urlpatterns = [
-    path('gacha/', views.GachaView.as_view(), name='gacha'),
+    path('', views.GachaView.as_view(), name='gacha'),
     path('result/', views.GachaResultView.as_view(), name='result'),
     path('list/', views.GachaListView.as_view(), name='list'),
 ]
