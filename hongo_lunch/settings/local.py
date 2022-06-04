d5138789c3c13ed3d3786052919098a5f775e93b
tani-cat <tani.tvet2749@gmail.com>
2022-06-04 19:30:53 +0900

    update readme for contributing

---
+++ hongo_lunch/settings/local.py
@@ -0,0 +1,28 @@
+import os
+
+from .base import *
+from .secret_key import _SECRET_KEY_CODE
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+# SECRET_KEYの設定についてはREADMEを参照
+SECRET_KEY = _SECRET_KEY_CODE
+
+# settings.pyからそのままコピー
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# 以下は共通項目への追加
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
