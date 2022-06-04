62af991d03d41fe0000aaa17d6ca945c352e98a4
tani-cat <tani.tvet2749@gmail.com>
2022-06-04 15:21:08 +0900

    add release note pages

--- hongo_lunch/settings/base.py
+++ hongo_lunch/settings/base.py
@@ -22,14 +22,15 @@ BASE_DIR = Path(__file__).parent.parent.parent
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.admin',
+    'lunch_gacha',
+    'release_manager',
+    'bootstrap4',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap4',
-    'lunch_gacha',
 ]
 
 MIDDLEWARE = [
